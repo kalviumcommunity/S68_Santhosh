6c471db1de3a9de04af9a0c11e1797b97d923da7
SANTHOSH746 <santhoshrca2006@gmail.com>
2025-02-04 15:41:19 +0530

    fc

---
+++ index.js
@@ -0,0 +1,26 @@
+// index.js
+const express = require('express');
+const { connectDB } = require('./Database/db');
+
+const app = express();
+
+require('dotenv').config();
+const port = process.env.PORT || 8080;
+const url = process.env.db_url;
+
+
+app.listen(port, async() => {
+
+  try{
+    await connectDB(url);
+    console.log(`Server is running on port ${port}`);
+  }
+  catch(error){
+    console.error(error);
+  }
+});
+
+
+app.get('/', (req, res) => {    
+    res.send('Hello World!');
+});
