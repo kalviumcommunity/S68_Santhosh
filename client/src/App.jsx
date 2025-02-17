7f58f373f61dd986c416ae7332c18bf4032e80aa
SANTHOSH746 <santhoshrca2006@gmail.com>
2025-02-17 09:42:55 +0530

    fc

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to ASAP Project</h1>
+      <p>This project is focused on building an advanced solution for an interactive web application that efficiently manages data using MongoDB and Express.js on the backend while providing a seamless user experience with React on the frontend. The goal is to create a responsive and user-friendly platform that enables users to perform CRUD operations, view real-time updates, and interact with stored data effortlessly.</p>
+    </div>
+  );
+}
+
+export default App;
